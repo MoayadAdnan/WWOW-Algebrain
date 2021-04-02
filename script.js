203fed56c3597a049e265a890fdcd660d2345560
MoayadAdnan <79250769+MoayadAdnan@users.noreply.github.com>
2021-04-02 20:41:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
+
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
